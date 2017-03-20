bb329add3bd3514fd89458dcb2e701db504dda0c
Asandolo <asandolo3@gmail.com>
2017-03-20 16:28:48 +0100

    go

--- InscriptionSport/src/xyz/teamkasand/Interface/frame/CompFrame.java
+++ InscriptionSport/src/xyz/teamkasand/Interface/frame/CompFrame.java
@@ -5,19 +5,26 @@
  */
 package xyz.teamkasand.Interface.frame;
 
-import java.awt.Graphics;
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import javax.swing.JCheckBox;
+import javafx.util.converter.LocalDateStringConverter;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneLayout;
 import xyz.teamkasand.Competition;
-import xyz.teamkasand.Equipe;
 import xyz.teamkasand.Inscriptions;
 
 /**
@@ -32,13 +39,14 @@ public class CompFrame extends JFrame {
         
         String[] header = {"#","Nom","Date de cloture","En equipe ?"};
         
+        JFrame th = this;
+        
         ArrayList<Competition> comp = i.getCompetitionsInArray();
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/YYYY");
         
         Object[][] datas = new Object[comp.size()][];
         for (int j = 0 ; j<comp.size(); j++) {
             Competition c = comp.get(j);
-            
             
             
             datas[j] = new Object[4];
@@ -48,13 +56,75 @@ public class CompFrame extends JFrame {
             datas[j][3] = c.estEnEquipe()?"Oui":"Non";
         }
         
+            JButton btn_retour = new JButton("Retour");
+            JButton btn_create = new JButton("Créé une competition");
+            
+            btn_retour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                th.dispose();
+            }
+        });
+            
+            btn_create.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean ee = false;
+                JTextField nom = new JTextField();
+                JSpinner anne = new JSpinner();
+                JSpinner moi = new JSpinner();
+                JSpinner jour = new JSpinner();
+                Object[] ob1 = {
+                    "nom",nom
+                };
+             
+            
+                
+                
+                Object[] ob2 = {
+                    "Année date de cloture",anne,
+                    "Mois date de cloture",moi,
+                    "Jour date de cloture",jour
+                };
+                
+                int j = JOptionPane.showConfirmDialog(th, ob1, "Ajouter une competition",JOptionPane.OK_CANCEL_OPTION);
+                if (j == JOptionPane.OK_OPTION) {
+                    int k = JOptionPane.showConfirmDialog(th, "En equipe ? ", "En equipe ?", JOptionPane.YES_NO_CANCEL_OPTION);
+                    if (k == 0) {
+                        ee = true;
+                        int m = JOptionPane.showConfirmDialog(th, ob2, "Ajouter une competition",JOptionPane.OK_CANCEL_OPTION);
+                        if (m == JOptionPane.OK_OPTION)  {
+                            LocalDate date = LocalDate.of((int)anne.getValue(), (int)moi.getValue(), (int)jour.getValue());
+                            if (i.BDCompetition(nom.getText(), date, ee)) {
+                                th.dispose();
+                                th.setVisible(true);
+                            }
+                        }
+                    }else if (k == 1){    
+                     ee = false;
+                        int o = JOptionPane.showConfirmDialog(th, ob2, "Ajouter une competition",JOptionPane.OK_CANCEL_OPTION);
+                        if (o == JOptionPane.OK_OPTION)  {
+                            LocalDate date = LocalDate.of((int)anne.getValue(), (int)moi.getValue(), (int)jour.getValue());
+                            if (i.BDCompetition(nom.getText(), date, ee)) {
+                                JOptionPane.showMessageDialog(th, "La compet à bien été créée", "OK", JOptionPane.INFORMATION_MESSAGE);
+                                th.dispose();
+                                th.setVisible(true);
+                            }else{
+                                JOptionPane.showMessageDialog(th, "Une erreur est survenue ! Merci de contacter votre administrateur", "ERROR", JOptionPane.ERROR_MESSAGE);
+                            }
+                        }
+                    }
+                    
+                }
+            }
+        });
         
          JTable table = new JTable(datas, header);
          table.setEnabled(false);
          
-         this.p = new JScrollPane(table);
-         this.p.setLayout(new ScrollPaneLayout());
-         
-         this.setContentPane(p);
+        this.setLayout(new BorderLayout());
+        this.add(btn_retour, BorderLayout.NORTH);
+        this.add(btn_create, BorderLayout.SOUTH);
+        this.add(new JScrollPane(table), BorderLayout.CENTER);
     } 
 }
